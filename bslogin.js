39cc246f87ac7239d222da3c1c3ebeca891140fa
Jorge <jorandho@gmail.com>
2022-08-29 11:49:08 -0500

    Script de js desde archivo externo

---
+++ bslogin.js
@@ -0,0 +1,12 @@
+const user = "georgieH"
+const pw = 1234
+function verificar(){
+    let usuario = document.getElementById("usuario").value
+    let clave = document.getElementById("clave").value
+            
+    if(usuario == user && clave == pw){
+        alert("Login exitoso")
+    }else{
+        alert("Revise los datos ingresados")
+    }
+}
